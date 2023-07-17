19dacaa7c8bc6b2889f204f06444b9827451d988
Tommy Trinh <tommy.trinh@pm.me>
2023-07-17 17:21:48 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+              'barlow': ['Barlow Semi Condensed', 'Helvetica']
+            },
+            colors: {
+                'moderate-violet': 'hsl(263, 55%, 52%)',
+                'very-dark-grayesh-blue': 'hsl(217, 19%, 35%)',
+                'very-dark-blackish-blue': 'hsl(219, 29%, 14%)',
+                'light-gray': 'hsl(0, 0%, 81%)',
+                'light-grayish-blue': 'hsl(210, 46%, 95%)',
+            },
+        },
+    },
+    plugins: [],
+};
